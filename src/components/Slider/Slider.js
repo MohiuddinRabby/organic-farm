10ae6d8b16008f687332394bce6d0621a9d954dc
rabbymohiuddin <rabbymohiuddin@gmail.com>
2020-10-01 12:12:28 +0600

    slider initial done

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -1,7 +1,7 @@
 import React from "react";
+import { Fade, Zoom, Slide } from "react-reveal";
 import "./Slider.css";
 const Slider = () => {
-
   return (
     <div
       id="carouselExampleCaptions"
@@ -9,69 +9,67 @@ const Slider = () => {
       data-ride="carousel"
     >
       <div className="carousel-inner">
-        <div className="carousel-item carousel-image-1 active">
-          <div className="dark-overlary">
-            <div className="container">
-              <div className="carousel-caption d-none d-sm-block">
-                <div className="title-box">
-                  <h5
-                    className="title-one"
-                  >
-                    High-Quality Organic Products
-                  </h5>
-                  <h5
-                    className="title-two"
-                  >
-                    VEGETABLES
-                  </h5>
-                  <h5
-                    className="title-three"
-                  >
-                    WITHOUT HARMFUL ADDITIVES
-                  </h5>
+        <Fade>
+          <div className="carousel-item carousel-image-1 active">
+            <div className="dark-overlary">
+              <div className="container">
+                <div className="carousel-caption d-none d-sm-block">
+                  <div className="title-box">
+                    <Slide left>
+                      <h5 className="title-one">
+                        High-Quality Organic Products
+                      </h5>
+                    </Slide>
+                    <Zoom>
+                      <h5 className="title-two">VEGETABLES</h5>
+                    </Zoom>
+                    <Slide right>
+                      <h5 className="title-three">WITHOUT HARMFUL ADDITIVES</h5>
+                    </Slide>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
-        <div className="carousel-item carousel-image-2">
-          <div className="dark-overlary">
-            <div className="container">
-              <div className="carousel-caption d-none d-sm-block">
-                <div className="title-box">
-                  <h5
-                    className="title-one"
-                  >
-                    Fresh Organic Produce
-                  </h5>
-                  <h5
-                    className="title-two"
-                  >
-                    FRUITS
-                  </h5>
-                  <h5
-                    className="title-three"
-                  >
-                    WITH LOTS OF VITAMINS
-                  </h5>
+          <div className="carousel-item carousel-image-2">
+            <div className="dark-overlary">
+              <div className="container">
+                <div className="carousel-caption d-none d-sm-block">
+                  <div className="title-box">
+                    <Slide left>
+                      <h5 className="title-one">Fresh Organic Produce</h5>
+                    </Slide>
+                    <Zoom>
+                      <h5 className="title-two">FRUITS</h5>
+                    </Zoom>
+                    <Slide right>
+                      <h5 className="title-three">WITH LOTS OF VITAMINS</h5>
+                    </Slide>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
-        <div className="carousel-item carousel-image-3">
-          <div className="dark-overlary">
-            <div className="container">
-              <div className="carousel-caption d-none d-sm-block">
-                <div className="title-box">
-                  <h5 className="title-one">Welcome to Our Farm</h5>
-                  <h5 className="title-two">PRODUCTS</h5>
-                  <h5 className="title-three">GROWN WITH LOVE</h5>
+          <div className="carousel-item carousel-image-3">
+            <div className="dark-overlary">
+              <div className="container">
+                <div className="carousel-caption d-none d-sm-block">
+                  <div className="title-box">
+                    <Slide left>
+                      <h5 className="title-one">Welcome to Our Farm</h5>
+                    </Slide>
+                    <Zoom>
+                      <h5 className="title-two">PRODUCTS</h5>
+                    </Zoom>
+                    <Slide right>
+                      <h5 className="title-three">GROWN WITH LOVE</h5>
+                    </Slide>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
+        </Fade>
       </div>
       <a
         className="carousel-control-prev"
